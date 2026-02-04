d79a8bb69552e4cd815d310b24579205a3aa1960
宋启维 <9055423@qq.com>
2026-02-04 20:43:48 +0800

    1

--- frontend/src/views/AdminOrders.vue
+++ frontend/src/views/AdminOrders.vue
@@ -39,7 +39,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="order in orders" :key="order.id">
+              <tr v-for="order in filteredOrders" :key="order.id">
                 <td>{{ order.id }}</td>
                 <td>{{ order.user_id }}</td>
                 <td>¥{{ order.totalPrice.toFixed(2) }}</td>
